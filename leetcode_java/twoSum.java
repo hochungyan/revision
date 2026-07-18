396ec86533707c4fb4608a623ccf1849b7fd9551
hochungyan <hochungyan@live.hk>
2026-07-18 18:30:43 +0800

    two Sum

---
+++ leetcode_java/twoSum.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class twoSum {
+    public int[] twosum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        int strlength = nums.length;
+        for (int i = 0; i < strlength; i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+
+public static void main(String[] args){
+    twoSum sol = new twoSum();
+    int[]test1 = {2,7,11,15};
+    int target1 = 9;
+    System.out.println(Arrays.toString(sol.twosum(test1,target1)));
+    }
+}
