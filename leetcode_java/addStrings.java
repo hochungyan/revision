4bea094216587f1bb944def93413c62d3af12783
hochungyan <hochungyan@live.hk>
2026-07-20 02:15:34 +0800

    firstg attempt

---
+++ leetcode_java/addStrings.java
@@ -0,0 +1,19 @@
+public class addStrings{
+    public String addString(String num1, String num2){
+        // int Num1 = Integer.parseInt(num1);
+        // int Num2 = Integer.parseInt(num2);
+        // return Integer.toString(Num1 + Num2);
+
+    }
+
+    public static void main(String[] args){
+        addStrings sol = new addStrings();
+        String test1 = "11";
+        String test2 = "123";
+        System.out.println(sol.addString(test1, test2));
+    }    
+
+
+    
+
+}
